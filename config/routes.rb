bef73d561463d944c985b0c4616646625c117052
okayu322434 <va.food.y.owari1006@gmail.com>
2020-09-14 23:31:26 +0900

    テストの実装改改
    https://gyazo.com/11076f1e425af49219c6c39051dff4c8

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :users, only: [:sign_up]
-  # get 'posts', to: 'items#index'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -114,12 +114,10 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
 
-    it "重複したemailが存在する場合登録できないこと" do
-      @user.save
-      another_user = FactoryBot.build(:user)
-      another_user.email = @user.email
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    it "emailの中に＠が含まれていなければ登録できない" do
+      @user.email = include"@"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
           end
   end
 

e94374e0fa9bc7671d9fa3e8f7665221a7a02c27
okayu322434 <va.food.y.owari1006@gmail.com>
2020-10-16 17:57:05 +0900

    内容編集改

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,4 +1,6 @@
 class GoodsController < ApplicationController
+  before_action :set_good, only: [:show, :edit, :update]
+
   def index
     @goods = Good.all
   end
@@ -16,29 +18,23 @@ class GoodsController < ApplicationController
   end
 
   def show
-    # @goods = Good.all
-    @good = Good.find(params[:id])
   end
 
   def edit
-    @good = Good.find(params[:id])
   end
 
   def update
-    @good = Good.find(params[:id])
     @good.update(goods_params)
     redirect_to action: :show
-  end
-
-  def destroy
-    @good = Good.find(params[:id])
-    @good.destroy
-    redirect_to root_path
   end
 
   private
   def goods_params
     params.require(:good).permit(:image, :name, :comment, :price, :category_id, :status_id, :delivery_id, :area_id, :shippingdate_id).merge(user_id: current_user.id)
+  end
+
+  def set_good
+    @good = Good.find(params[:id])
   end
 
 

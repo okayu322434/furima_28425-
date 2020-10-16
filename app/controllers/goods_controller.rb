33bea276390c3b51b4f979b8c26ee9f973a767af
okayu322434 <va.food.y.owari1006@gmail.com>
2020-10-16 16:32:58 +0900

    内容編集・消去

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -15,9 +15,32 @@ class GoodsController < ApplicationController
     end
   end
 
+  def show
+    # @goods = Good.all
+    @good = Good.find(params[:id])
+  end
+
+  def edit
+    @good = Good.find(params[:id])
+  end
+
+  def update
+    @good = Good.find(params[:id])
+    @good.update(goods_params)
+    redirect_to action: :show
+  end
+
+  def destroy
+    @good = Good.find(params[:id])
+    @good.destroy
+    redirect_to root_path
+  end
+
   private
   def goods_params
     params.require(:good).permit(:image, :name, :comment, :price, :category_id, :status_id, :delivery_id, :area_id, :shippingdate_id).merge(user_id: current_user.id)
   end
+
+
 end
 
